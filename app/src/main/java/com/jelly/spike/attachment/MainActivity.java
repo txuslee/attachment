1cb20d9ccff1a7b85394ce64358804ae88b169c4
Jesus Liarte <jesus.liarte@zyncro.com>
2015-04-24 09:11:16 +0200

    Added methods expand/collapse for future reference

--- app/src/main/java/com/jelly/spike/attachment/MainActivity.java
+++ app/src/main/java/com/jelly/spike/attachment/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 
 import com.jelly.spike.attachment.adapter.impl.AttachmentIconAdapter;
 import com.jelly.spike.attachment.adapter.impl.RowDependentIconAdapter;
@@ -29,6 +31,57 @@ public class MainActivity extends ActionBarActivity {
     protected ActionIconGridView attachmentActionsGridView;
 
     private boolean isAttachmentActionShown = false;
+
+    public static void expand(final View v) {
+        v.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        final int targetHeight = v.getMeasuredHeight();
+
+        v.getLayoutParams().height = 0;
+        v.setVisibility(View.VISIBLE);
+        final Animation a = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                v.getLayoutParams().height = interpolatedTime == 1
+                        ? ViewGroup.LayoutParams.WRAP_CONTENT
+                        : (int) (targetHeight * interpolatedTime);
+                v.requestLayout();
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms
+        a.setDuration((int) (targetHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
+    }
+
+    public static void collapse(final View v) {
+        final int initialHeight = v.getMeasuredHeight();
+
+        final Animation a = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if (interpolatedTime == 1) {
+                    v.setVisibility(View.GONE);
+                } else {
+                    v.getLayoutParams().height = initialHeight - (int) (initialHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms
+        a.setDuration((int) (initialHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,8 +139,12 @@ public class MainActivity extends ActionBarActivity {
     @OnClick(R.id.btn_attachFile)
     public void onAttachmentClick(final View view) {
         if (this.isAttachmentActionShown) {
+            //this.hide(this.attachmentActionsGridView);
+            //this.collapse(this.attachmentActionsGridView);
             this.hideAttachmentActions();
         } else {
+            //this.reveal(this.attachmentActionsGridView);
+            //this.expand(this.attachmentActionsGridView);
             this.revealAttachmentActions();
         }
         this.isAttachmentActionShown = !this.isAttachmentActionShown;
@@ -143,5 +200,4 @@ public class MainActivity extends ActionBarActivity {
         }
         view.setVisibility(View.VISIBLE);
     }
-
 }
